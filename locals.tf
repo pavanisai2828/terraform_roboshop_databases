938627737348da5c0934071cc13d39e3cda0278d
pavanisai2828 <pavani.sai1313@gmailcom>
2026-07-31 05:33:48 +0530

    second code added

--- locals.tf
+++ locals.tf
@@ -2,19 +2,19 @@ locals {
   databases={
     mongodb={
         sg_id= local.mongodb_sg_id
-        script= mongodb-bootsrap.sh
+        script= mongodb-bootstrap.sh
     }
     redis={
         sg_id= local.redis_sg_id
-        script=redis-bootsrap.sh
+        script=redis-bootstrap.sh
     }
     mysql={
         sg_id= local.mysql_sg_id
-        script=mysql-bootsrap.sh
+        script=mysql-bootstrap.sh
     }
     rabbitmq={
         sg_id= local.rabbitmq_sg_id
-        script=rabbitmq-bootsrap.sh
+        script=rabbitmq-bootstrap.sh
     }
   }
 }
